78466dc17b2f7104757309de0554a9b5f6dd32ea
Farukhrana14 <farukhrana14@gmail.com>
2021-05-13 08:22:40 +0600

    Server ready-going to deploy

--- index.js
+++ index.js
@@ -3,27 +3,32 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-// const uri = `mongodb+srv://ExDbUser:cigUtQOzkJGaSq5i@cluster0.kjqp5.mongodb.net/iccTeamPlayers?retryWrites=true&w=majority`;
-const uri = "mongodb+srv://ExDbUser:cigUtQOzkJGaSq5i@cluster0.kjqp5.mongodb.net/iccTeamPlayers?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cohpt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 //App & Middleware
 const app = express();
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-//API
-app.get('/', (req, res)=> {
-    res.send('403 - Direct Access is Denied')
-});
+
+
 
 //Connections
 client.connect(err => {
-    const playersCollection = client.db("iccTeamPlayers").collection("playersData");
+    console.log('db connection error:', err);
+    const playersCollection = client.db(`${process.env.DB_NAME}`).collection('playersdata');
     
-    //API
+
+    //API ROOT
+    app.get('/', (req, res)=> {
+        res.send('403 - Direct Access is Denied')
+    });
+
+    //API Get All
     app.get('/playersall', (req, res)=> {
         playersCollection.find({})
             .toArray((err, documents) => {
@@ -42,46 +47,6 @@ client.connect(err => {
 
 
 
-
-// client.connect(err => {
-//   const playersCollection = client.db("iccTeamPlayers").collection("playersData");
-
-//  //API to fetch all data
-//  app.get('/playersdata', (req, res)=> {
-//     playersCollection.find({'name': 'Shakib'})
-//         .toArray((err, documents)=> {
-//             return res.status(200).send(documents);
-            
-//         })
-// });
-
-//   console.log('db connected');
-// });
-
-
-// client.connect(err => {
-//     console.log('db connection error:', err);
-//     const playersCollection = client.db('iccTeamPlayers').collection("playersData");
-    
-    // //API to fetch all data
-    // app.get('/playersdata', (req, res)=> {
-    //     playersCollection.find({})
-    //         .toArray((err, documents)=> {
-    //             res.send(documents);
-    //         })
-    // });
-    
-//     //API to add data
-//     app.post('/addplayers', (req, res) => {
-//         const data = req.body;
-//         console.log(data);
-//     })
-    
-    
-    
-    
-//     console.log('db connected');
-// });
 
 
 
